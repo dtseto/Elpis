3376d572412a6b1bfde22b44ece2277d9e54e4ec
David Seto <67933197+dtseto@users.noreply.github.com>
2025-08-22 11:55:17 -0700

    dotnetfram
    dotnetframework 4.6 to 4.8

---
+++ Elpis/ReleaseData.cs
@@ -0,0 +1,11 @@
+namespace Elpis
+{
+    public class ReleaseData
+    {
+        public const string BassRegEmail = "";
+        public const string BassRegKey = "";
+        public const string UpdateBaseUrl = @"";
+        public const string UpdateConfigFile = "";
+        public const string AnalyticsPostURL = @"";
+    }
+}
